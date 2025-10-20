800db685546cb816b0c13a3751b254f754c00957
Tom Kooij <tjwckooij@gmail.com>
2025-10-20 09:36:50 +0200

    20/10/2025
    - Added my 3D test project to this repository

---
+++ 3d_test/characters/scripts/player.gd
@@ -0,0 +1,66 @@
+extends CharacterBody3D
+
+@onready var camera_pivot : Node3D = %CameraPivot
+@onready var camera : Camera3D = %Camera3D
+@onready var player_model : Node3D = %player_model
+
+@export_category("Camera")
+@export_range(0.0, 1.0) var mouse_sensitivity : float = 0.25
+
+@export_category("Movement")
+@export var move_speed : float = 8.0
+@export var acceleration : float = 20.0
+@export var rotation_speed : float = 10.0
+
+var camera_input_direction : Vector2 = Vector2.ZERO
+var last_movement_direction : Vector3 = Vector3.BACK
+
+
+func _physics_process(delta: float) -> void:
+	camera_pivot.rotation.x += camera_input_direction.y * delta
+	camera_pivot.rotation.x = clampf(camera_pivot.rotation.x, -PI / 6.0, PI / 3.0)
+	camera_pivot.rotation.y -= camera_input_direction.x * delta
+	
+	camera_input_direction = Vector2.ZERO
+
+	_movement(delta)
+
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	if event.is_action_pressed("ui_cancel"):
+		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+
+	
+func _unhandled_input(event: InputEvent) -> void:
+	var is_camera_motion := (
+		event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED
+	)
+	if is_camera_motion:
+		camera_input_direction = event.screen_relative * mouse_sensitivity
+		
+
+func _movement(delta: float) -> void:
+	var raw_input : Vector2 = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var forward : Vector3 = camera.global_basis.z
+	var right : Vector3 = camera.global_basis.x
+
+	var move_direction : Vector3 = forward * raw_input.y + right * raw_input.x
+	move_direction.y = 0.0
+	move_direction = move_direction.normalized()
+
+	velocity = velocity.move_toward(move_direction * move_speed, acceleration * delta)
+	move_and_slide()
+	
+	if move_direction.length() > 0.2:
+		last_movement_direction = move_direction
+	var target_angle : float = Vector3.BACK.signed_angle_to(last_movement_direction, Vector3.UP)
+	player_model.global_rotation.y = lerp_angle(player_model.rotation.y, target_angle, rotation_speed * delta)
+	
+	var ground_speed : float = velocity.length()
+	var animation_player : AnimationPlayer = player_model.get_node("AnimationPlayer")
+	if ground_speed > 0.0:
+		animation_player.play("player_anims/Walking")
+	else:
+		animation_player.play("player_anims/Idle")
